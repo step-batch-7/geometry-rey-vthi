ed6d1cbb6bde8c91eabe7394164bf35b5a2c5877
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2019-12-13 10:16:27 +0530

    improved the way of assignment to the object

--- src/line.js
+++ src/line.js
@@ -4,8 +4,8 @@ const isTwoPointsEqual = function(point1, point2) {
 
 class Line {
   constructor(endA, endB) {
-    this.endA = endA;
-    this.endB = endB;
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
   toString() {
     return `Line (${this.endA.x},${this.endA.y}) (${this.endB.x},${this.endB.y})`;
