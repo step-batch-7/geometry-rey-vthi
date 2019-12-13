8ec078bb3c40d25b7921e6dda3b6f183788364ea
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2019-12-13 16:05:18 +0530

    renamed ,refactorised
    renamed isTwoEqualPoints to areEqualPoints
    removed extra testcases
    improved the way of assigning the feilds

--- src/line.js
+++ src/line.js
@@ -1,11 +1,11 @@
-const isTwoPointsEqual = function(point1, point2) {
+const arePointsEqual = function(point1, point2) {
   return point1.x === point2.x && point1.y === point2.y;
 };
 
 class Line {
   constructor(endA, endB) {
-    this.endA = { ...endA };
-    this.endB = { ...endB };
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
   toString() {
     return `Line (${this.endA.x},${this.endA.y}) (${this.endB.x},${this.endB.y})`;
@@ -13,10 +13,10 @@ class Line {
   isEqualTo(other) {
     return (
       other instanceof Line &&
-      isTwoPointsEqual(this.endA, other.endA) &&
-      isTwoPointsEqual(this.endB, other.endB)
+      arePointsEqual(this.endA, other.endA) &&
+      arePointsEqual(this.endB, other.endB)
     );
   }
 }
 
-module.exports = { Line: Line, isTwoPointsEqual: isTwoPointsEqual };
+module.exports = Line;
--- test/testLine.js
+++ test/testLine.js
@@ -1,52 +1,50 @@
 const assert = require("assert");
-const { Line, isTwoPointsEqual } = require("../src/line.js");
+const Line = require("../src/line.js");
 
-describe("line representation ,to string", function() {
-  it("should give the representation of the given positive number line", function() {
-    const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
-    const expected = `Line (2,2) (4,3)`;
-    assert.strictEqual(line.toString(), expected);
-  });
-  it("should give the representation of the given negative number line", function() {
-    const line = new Line({ x: 3, y: 3 }, { x: 8, y: 2 }, { x: 3, y: 3 });
-    const expected = `Line (3,3) (8,2)`;
-    assert.strictEqual(line.toString(), expected);
+describe("line representation", function() {
+  describe("toString", function() {
+    it("should give the representation of the given line [positive points]", function() {
+      const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
+      const expected = `Line (2,2) (4,3)`;
+      assert.strictEqual(line.toString(), expected);
+    });
+    it("should give the representation of the given line [negative points]", function() {
+      const line = new Line({ x: -3, y: 3 }, { x: -8, y: 2 });
+      const expected = `Line (-3,3) (-8,2)`;
+      assert.strictEqual(line.toString(), expected);
+    });
+
+    it("should give the representation of the given line, when extra points are given", function() {
+      const line = new Line({ x: 3, y: 3 }, { x: 8, y: 2 }, { x: 3, y: 2 });
+      const expected = `Line (3,3) (8,2)`;
+      assert.strictEqual(line.toString(), expected);
+    });
   });
 });
 
-describe("line representation, is equal function ", function() {
-  it("should equate its own property with given other similar property", function() {
-    const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
-    const other = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
-    assert.ok(line.isEqualTo(other));
-  });
-  it("should equate its own property with given other similar property[negative number]", function() {
-    const line = new Line({ x: -4, y: 2 }, { x: 4, y: -3 });
-    const other = new Line({ x: -4, y: 2 }, { x: 4, y: -3 });
-    assert.ok(line.isEqualTo(other));
-  });
+describe("line representation", function() {
+  describe("isEqualTo", function() {
+    it("should equate its own instance with given other similar instance", function() {
+      const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
+      const other = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
+      assert.ok(line.isEqualTo(other));
+    });
+    it("should equate its own instance with other similar instance[negative points]", function() {
+      const line = new Line({ x: -4, y: 2 }, { x: 4, y: -3 });
+      const other = new Line({ x: -4, y: 2 }, { x: 4, y: -3 });
+      assert.ok(line.isEqualTo(other));
+    });
 
-  it("should check the instance of the Object, where both should be same", function() {
-    const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
-    const other = { endA: { x: 2, y: 2 }, endB: { x: 4, y: 3 } };
-    assert.ok(!line.isEqualTo(other));
-  });
+    it("should check the instance of the Object, where other should have similar instance", function() {
+      const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
+      const other = { endA: { x: 2, y: 2 }, endB: { x: 4, y: 3 } };
+      assert.ok(!line.isEqualTo(other));
+    });
 
-  it("should check the instance of the Object, where other is just an empty object", function() {
-    const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
-    const other = {};
-    assert.ok(!line.isEqualTo(other));
-  });
-});
-
-describe("isTwoPointsEqual function", function() {
-  it("should check both given points are equal[positive points]", function() {
-    assert.ok(isTwoPointsEqual({ x: 3, y: 1 }, { x: 3, y: 1 }));
-  });
-  it("should check both given points are equal[negative points]", function() {
-    assert.ok(isTwoPointsEqual({ x: -3, y: -1 }, { x: -3, y: -1 }));
-  });
-  it("should return false whenever the points are not equal", function() {
-    assert.ok(!isTwoPointsEqual({ x: 2, y: 1 }, { x: 2, y: 2 }));
+    it("should check the instance of the Object, when other is just an empty object", function() {
+      const line = new Line({ x: 2, y: 2 }, { x: 4, y: 3 });
+      const other = {};
+      assert.ok(!line.isEqualTo(other));
+    });
   });
 });
